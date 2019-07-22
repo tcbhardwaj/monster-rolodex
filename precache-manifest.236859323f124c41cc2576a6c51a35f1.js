cfa1e9539c9407e7a5596c7234abbcb4be00af8c
collab-virchatri <trilokc@virtusa.com>
2019-07-22 22:09:34 +0530

    Updates

---
+++ precache-manifest.236859323f124c41cc2576a6c51a35f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa66e718583da0d4f81bb7f26c49eaa",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dc6cbabcc30cc20fdee9",
+    "url": "/monster-rolodex/static/css/main.f81bd091.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dc6cbabcc30cc20fdee9",
+    "url": "/monster-rolodex/static/js/main.f494869f.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
